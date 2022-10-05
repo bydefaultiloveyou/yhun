da0e6e43508bafbce8c9ce8bc02e529ea81a54c5
Miko Meysa Bima <meysa@pop-os.localdomain>
2022-10-05 19:55:40 +0700

    add feature download music

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import WAWebJS, { Client, LocalAuth } from "whatsapp-web.js"
 import MessagesHanlder from "./handler/masages"
 import { QrHandler } from "./handler/qr"
 import { ReadyHandler } from "./handler/ready"
+import express, {Express} from "express";
+
+const app : Express = express()
 
 export const client : WAWebJS.Client = new Client({
   authStrategy: new LocalAuth(),
@@ -12,7 +15,10 @@ export const client : WAWebJS.Client = new Client({
 })
 
 client.on("message", MessagesHanlder)
+
 client.on("qr", QrHandler)
 client.on("ready", ReadyHandler)
 
 client.initialize()
+
+app.listen(3000, () => console.log('running oi nande nande'))
